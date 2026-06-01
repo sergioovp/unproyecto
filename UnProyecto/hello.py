a9debffaa3bd5a2fd7c94501f05bdf9a5d93a0a5
Sergio <sergiovalenciapastrana@gmail>
2026-05-31 19:29:23 -0500

    Agregar saludoespecial

--- UnProyecto/hello.py
+++ UnProyecto/hello.py
@@ -5,3 +5,8 @@ def despedida(nombre):
     return f"Adios,{nombre}"
 if __name__ =="__main__":
     print(saludar("Unmsm"))
+
+def saludoespecial(nombre):
+    return f"Bienvenido, {nombre}. Somos la mejor de UNMSM"
+if __name__ =="__main__":
+    print(saludar("Unmsm"))
