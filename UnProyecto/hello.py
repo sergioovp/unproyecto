f0a9c67cd2d51904f53c2ee47ae9a43f8621c168
Sergio <sergiovalenciapastrana@gmail>
2026-06-08 16:11:25 -0500

    Hello.py modificacion

--- UnProyecto/hello.py
+++ UnProyecto/hello.py
@@ -7,7 +7,7 @@ if __name__ =="__main__":
     print(saludar("Unmsm"))
 
 def saludoespecial(nombre):
-    return f"Bienvenido, {nombre}. Somos la mejor de UNMSM"
+    return f"Bienvenido, {nombre}. Somos la m"
 if __name__ =="__main__":
     print(saludar("Unmsm"))
 def nuevanueva(nombre):
